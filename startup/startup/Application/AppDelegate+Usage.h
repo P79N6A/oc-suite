b3ef9e604bc9b2df0f57682d6a2fb6c5f5f3d82c
7 <fengzilijie@qq.com>
2018-04-28 16:34:39 +0800

    +

---
+++ startup/startup/Application/AppDelegate+Usage.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate+Usage.h
+//  startup
+//
+//  Created by 7 on 2018/4/28.
+//  Copyright © 2018 7. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate (Usage)
+
+@end
---
+++ startup/startup/Application/AppDelegate+Usage.m
@@ -0,0 +1,13 @@
+//
+//  AppDelegate+Usage.m
+//  startup
+//
+//  Created by 7 on 2018/4/28.
+//  Copyright © 2018 7. All rights reserved.
+//
+
+#import "AppDelegate+Usage.h"
+
+@implementation AppDelegate (Usage)
+
+@end
